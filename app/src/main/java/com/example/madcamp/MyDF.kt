cc0201348303bc568979d821e0e40c44b0da0c0a
chlehdwon <cookie000215@gmail.com>
2022-07-04 23:49:46 +0900

    final design done

---
+++ app/src/main/java/com/example/madcamp/MyDF.kt
@@ -0,0 +1,26 @@
+package com.example.madcamp
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class MyDF: DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.contact_mycard, container, false)
+    }
+
+    fun getInstance(): MyDF{
+        return MyDF()
+    }
+
+
+
+}
