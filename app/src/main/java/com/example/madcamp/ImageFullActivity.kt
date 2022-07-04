db589c6ed82f6226cc2038ecfd17a7dad5004347
chlehdwon <cookie000215@gmail.com>
2022-07-04 17:42:35 +0900

    tab1 intent done

--- app/src/main/java/com/example/madcamp/ImageFullActivity.kt
+++ app/src/main/java/com/example/madcamp/ImageFullActivity.kt
@@ -1,14 +1,22 @@
 package com.example.madcamp
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ImageButton
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ImageFullActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_full)
+
+        supportActionBar?.hide()
 
         val imagePath = intent.getStringExtra("path")
         val imageName = intent.getStringExtra("name")
@@ -17,5 +25,12 @@ class ImageFullActivity : AppCompatActivity() {
         Glide.with(this)
             .load(imagePath)
             .into(findViewById(R.id.imageView))
+
+        val dmButton : ImageButton = findViewById(R.id.btndm)
+        dmButton.setOnClickListener {
+            finish()
+
+        }
     }
+
 }
